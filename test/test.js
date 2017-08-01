c1de440f1a87c13326687f79db7d86b10055b2a1
Adeohluwa <adejumoadeoluwa@gmail.com>
2017-08-01 18:36:05 +0100

    fix error in first failing test

--- test/test.js
+++ test/test.js
@@ -2,5 +2,12 @@
 //and accuracy of an OOP Model of a bank account
 
 
-//fat arrows not allowed inside test suite
-//mochajs.org/#arrow-functions
+//fat arrows not allowed inside mocha test suite
+//see ---> mochajs.org/#arrow-functions
+
+const expect = require('chai').expect;
+
+describe('Bank Account Suite', function(){
+	it('Should Return 5000',function(){
+    	expect(Ade.balance()).to.equal(5000);})
+})
