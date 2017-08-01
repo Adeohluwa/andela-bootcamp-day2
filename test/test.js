1eb4833d0b075cdd7330a1cb8324bf620cd5337c
Adeohluwa <adejumoadeoluwa@gmail.com>
2017-08-01 20:15:45 +0100

    annotate test specs with comments

--- test/test.js
+++ test/test.js
@@ -10,21 +10,22 @@ const Ade = require("../bank.js").Ade;
 
 
 describe("Bank Account Suite", function(){
+	//assert that initial balance is 5000
 	describe("INITIAL account balance should be N5000", function() {
     	it("should be 5000", function() {
       expect(Ade._balance).to.equal(5000);
   	})});
-
+	//assert that the deposit method works correctly
   	describe("DEPOSIT method for class account", function() {
     	it("should return success msg", function() {
       expect(Ade.deposit(5000)).to.equal("TRANSACTION SUCCESSFUL: You have deposited 5000");
   	})});
-
+  	//assert that the withdraw method works correctly
   	describe("WITHDRAW method for class account", function() {
     	it("should return success msg", function() {
       expect(Ade.withdraw(5000)).to.equal("TRANSACTION SUCCESSFUL: You have withdrawn 5000");
   	})});
-
+  	//assert that balance method works correctly
   	describe("BALANCE method for class account", function() {
     	it("should return success msg", function() {
       expect(Ade.balance()).to.equal("TRANSACTION SUCCESSFUL: You have 5000 in your account");
