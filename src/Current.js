270b6049d1a517c96d604e8d813546dc6ae28431
Adeohluwa <adejumoadeoluwa@gmail.com>
2017-08-26 13:43:04 +0100

    move code & test to src directory

---
+++ src/Current.js
@@ -0,0 +1,28 @@
+// aaaaaaaaaaaa
+
+import Account from './Account';
+
+class CurrentAcc extends Account {
+  // withdrawal method for current account class
+  withdraw(amount) {
+    if (amount < (this.balance + 10000)) {
+      this.balance -= amount;
+      return `TRANSACTION SUCCESSFUL: You have withdrawn ${amount} in your account`;
+    }
+    return 'TRANSACTION ERROR!';
+  }
+
+  overdraft(amount) {
+    if (amount <= (this.balance * 3)) {
+      this.balance -= amount;
+      return `TRANSACTION SUCCESSFUL: You have accessed an overdraft of ${amount}`;
+    }
+    return 'TRANSACTION ERROR!';
+  }
+}
+
+
+// export an instance 
+
+const Tomi = new CurrentAcc('Israel Tomilola', 17, 1000000);
+export default Tomi;
---
+++ src/Savings.js
@@ -0,0 +1,21 @@
+// aaaaaaaaaaaaaaaa
+
+import Account from './Account';
+
+class SavingsAcc extends Account {
+  // withdraw method for saving account class
+  withdraw(amount) {
+    if (amount < (this.balance + 5000)) {
+      this.balance -= amount;
+      return `TRANSACTION SUCCESSFUL: You have withdrawn ${amount} in your account`;
+    }
+
+    return 'TRANSACTION ERROR!';
+  }
+}
+
+// export an instance 
+
+const Victoria = new SavingsAcc('Victoria Offoma', 19, 50000);
+
+export default Victoria;
